fbf579fbb86d378b1f6bad6aad719f1637244dff
gomoseky <matfeifcdc222@gmail.com>
2024-08-18 11:01:47 +0300

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import styles from "./Footer.module.scss";
+
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+            showText: true,
+            currentTextIndex: 0,
+        };
+        this.texts = [
+            `ЖДУ ДАМАГ ! ${this.props.username || '???'}`,
+            "?",
+            "Мой тг:@soonzins.",
+            "Мой дискорд сервер CALIPSO:https://discord.gg/"
+            // Добавьте сюда другие строки текста, которые вы хотите анимировать
+        ];
+        this.interval = null;
+    }
+
+    componentDidMount() {
+        this.startTextAnimation();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.username !== prevProps.username) {
+            // Update the first text with the new username
+            this.texts[0] = `ЖДУ ДАМАГ !! ${this.props.username || '???'}`;
+        }
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    startTextAnimation = () => {
+        this.interval = setInterval(() => {
+            this.setState({ showText: false });
+
+            setTimeout(() => {
+                this.setState(prevState => ({
+                    text: this.texts[prevState.currentTextIndex],
+                    currentTextIndex: (prevState.currentTextIndex + 1) % this.texts.length,
+                    showText: true,
+                }));
+            }, 500); // Duration of fade out
+        }, 2500); // Total duration for each text display (2 seconds display + 0.5 second fade out)
+    };
+
+    render() {
+        const { text, showText } = this.state;
+        return (
+            <footer className={styles.footer}>
+                <p className={`${styles.text} ${showText ? '' : styles.fadeOut}`}>
+                    <span>{text}</span>
+                </p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const id = '1233060358260916325' // Discord id
+
+export const github = 'https://github.com/Sayunaro' // Your github url
+
+export const theme = 'dark' // "dark" or "light"
